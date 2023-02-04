55203055c2b575bfb9f8d97dcca3ba59fca6dc02
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-02-04 07:24:37 +0000

    :sparkles: feat: add api endpoint for getting all standups
    Added the service, controller and route for a new endpoint to fetch all the standups.

--- src/routes/standup.route.ts
+++ src/routes/standup.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createStandup } from "../controllers/standup.controller";
+import { createStandup, getAllStandups } from "../controllers/standup.controller";
 
 const router = express.Router();
 
 router.post("/", createStandup);
+
+router.get("/", getAllStandups)
 
 export default router;
