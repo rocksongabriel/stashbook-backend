df65dc48a493b2d297e35333c95610de74b8a5fc
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-02-04 05:46:58 +0000

    feat: install body-parser and setup middleware with it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, { Express, Response, Request } from "express";
+import bodyParser from "body-parser";
 
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
 const host: string = "0.0.0.0";
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ data: "Welcome to the stashbook" });
