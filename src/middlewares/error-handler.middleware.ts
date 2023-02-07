37f89fe69af01de1c157cf898b66c789aa35b97c
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-02-07 06:41:15 +0000

    :sparkles: feat(error-handling): Introduce a middleware to handle errors.

---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,35 @@
+import { Prisma } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
+
+const BAD_REQUEST = StatusCodes.BAD_REQUEST;
+
+function handleError(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let customError = err;
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    return res.status(BAD_REQUEST).json({
+      message: err.message,
+      additionalInfo: err.meta,
+      status: BAD_REQUEST,
+    });
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    return res.status(BAD_REQUEST).json({
+      message: err.message,
+      status: BAD_REQUEST,
+    });
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    return res.status(BAD_REQUEST).json({
+      message: err.message,
+      status: BAD_REQUEST,
+    });
+  }
+  next(err);
+}
+
+export default handleError;
