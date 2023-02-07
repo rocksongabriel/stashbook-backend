8ddf91be0c3304c0888ee5baca2750d2074e2ba0
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-02-07 06:42:23 +0000

    :sparkles: feat(api): Add api endpoint to create a blocker.
    - Add a service to add a new blocker object to the database.
    - Add a controller and route to handle the request and response cycle.

---
+++ src/services/blocker.service.ts
@@ -0,0 +1,9 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const create = async (blocker: Prisma.BlockerCreateInput) => {
+  return await prisma.blocker.create({
+    data: blocker,
+  });
+};
